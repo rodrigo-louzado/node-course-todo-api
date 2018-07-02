e8a3c1cb127265d7ac0094cb178051c62d2d3cbe
Rodrigo Louzado <rc.louz@gmail.com>
2018-07-01 23:30:34 -0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -14,11 +14,22 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     })
     todo.save().then((doc) => {
-        res.status(200).send(doc);
+        res.send(doc);
     }, (e) => {
         res.status(400).send(e);
     })
 });
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos,
+            status: 'OK'
+        })
+    }, (e) => {
+        res.status(400).send(e);
+    })
+})
 
 
 app.listen(3000, () => {
